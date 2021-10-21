8e858494030b3aee7715a813b40f6ed70e575d2c
Lena Schön <lena.schoen@squer.at>
2021-10-21 19:35:02 +0200

    add files

--- index.js
+++ index.js
@@ -3,3 +3,25 @@
 
 // this is another comment
 // Lena, all your comments are nonsense
+
+import { compareAsc, format } from 'date-fns';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+format(new Date(2014, 1, 11), 'yyyy-MM-dd');
+//=> '2014-02-11'
+
+const dates = [
+  new Date(1915, 6, 2),
+  new Date(1931, 1, 11),
+  new Date(1959, 6, 10),
+];
+dates.sort(compareAsc);
+//=> [
+//   Wed Feb 11 1917 00:00:00,
+//   Mon Jul 10 1949 00:00:00,
+//   Sun Jul 02 1965 00:00:00
+// ]
+
+console.log('These are the dates:', dates);
